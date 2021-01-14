320b8c675e3a99578105dc938906da3d5479ab7a
Josphat <josphatkips@gmail.com>
2021-01-14 17:53:46 +0300

    updates

--- odoo_bulk_sms_kenya/models/sendtext.py
+++ odoo_bulk_sms_kenya/models/sendtext.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 
 class SendText(models.Model):
     _name='send.text'
-    sender_id=fields.Many2many('api.keys', required=True)
+    sender_id=fields.Many2one('sender.id', required=True)
     phone_number=fields.Text(required=True)
     text_message=fields.Text(required=True)
