6a61927aae249ee81c5b5cdd3b95df5a7776bf59
Josphat <josphatkips@gmail.com>
2021-01-14 14:03:10 +0300

    initial commit

---
+++ odoo_bulk_sms_kenya/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name':'Odoo Bulk SMS Kenya',
+    'summary':'Integrate bulk sms into odoo ERP in kenya',
+    'version':'12.0.1.0.0',
+    'category':'Tools',
+    'author':'Royce Technologies',
+    'maintainer':'Royce Technologies',
+    'company':'Royce Technologies',
+    'website':'https://roycetechnologies.co.ke',
+    'depends':['base'],
+    'data':[
+        ''
+    ],
+    'images':[],
+    'license':'AGPL-3',
+    'application':False,
+    'installable':True,
+    'auto_install':False
+
+
+}
---
+++ odoo_bulk_sms_kenya/models/senttext.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class SentText(models.Model):
+    _name='sent.text'
+    text_message=fields.Text(string="Messaqge", required=True)
+    sender_id=fields.Char(string="Sender Id", required=True)
+    phone_number=fields.Char(required=True)
+    status=fields.Char(required=True)
+    message_id=fields.Char()
+    response_code=fields.Char()
+    response_description=fields.Char()
+    network_id=fields.Char()
+    delivery_status=fields.Char()
+    delivery_description=fields.Char()
+    delivery_tat=fields.Char()
+    delivery_networkid=fields.Char()
+    delivery_time=fields.Char()
+    delivery_code=fields.Char()
+    delivery_network_id=fields.Char()
+    delivery_response_description=fields.Char()
